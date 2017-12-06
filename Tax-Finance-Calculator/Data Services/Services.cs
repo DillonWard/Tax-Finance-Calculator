a808453440cf1ec4e5e4d6cf5d1c6e40bd925554
Dillon Ward <Dillonward13@gmail.com>
2017-12-06 16:53:47 +0000

    trying to connect to azure

---
+++ Tax-Finance-Calculator/Data Services/Services.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Tax_Finance_Calculator.Data_Services
+{
+    class Services
+    {
+
+
+    }
+
+
+
+}
