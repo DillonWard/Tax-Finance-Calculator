9a4135a3242f3d6e2a8f79041d5d15557a60a37b
Dillon <Dillonward13@gmail.com>
2017-12-09 21:05:53 +0000

    can read and store data from azure

--- Tax-Finance-Calculator/ViewModel/TaxRateViewModel.cs
+++ Tax-Finance-Calculator/ViewModel/TaxRateViewModel.cs
@@ -9,7 +9,9 @@ namespace Tax_Finance_Calculator.ViewModel
 {
     class BracketViewModel
     {
-
+        string[] cNames = new string[5];
+        double[] cutOffs = new double[5];
+        double[] percents = new double[5];
         // Reads in the user input and assigns it to the BracketModel
 
         // Deduct the tax from gross income
